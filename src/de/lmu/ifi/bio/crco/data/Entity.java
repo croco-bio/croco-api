b96c082016a6785deb372a99d857f01741ecd80d
Robert Pesch <robert.pesch@bio.ifi.lmu.de>
2013-12-12 10:15:14 +0100

    Removed type field

--- src/de/lmu/ifi/bio/crco/data/Entity.java
+++ src/de/lmu/ifi/bio/crco/data/Entity.java
@@ -3,11 +3,8 @@ package de.lmu.ifi.bio.crco.data;
 public class Entity {
 	private String name;
 	private String identifier;
-	private String type;
 	
-	public String getType() {
-		return type;
-	}
+	
 	public String getName() {
 		return name;
 	}
@@ -22,18 +19,13 @@ public class Entity {
 		this.name = identifier;
 	}
 	
-	public Entity(String identifier, String name){
-		this.identifier = identifier.toUpperCase();
-		this.name = name;
-	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Entity(String identifier, String name, String type){
+	public Entity(String identifier, String name){
 		this.identifier = identifier.toUpperCase();
 		this.name = name;
-		this.type = type;
 	}
 
 	@Override
