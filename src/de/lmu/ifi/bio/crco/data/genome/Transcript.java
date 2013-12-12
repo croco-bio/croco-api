695c6f91bede7c48ed7ac8678e3f8bd4c75811b7
Robert Pesch <robert.pesch@bio.ifi.lmu.de>
2013-12-12 10:15:00 +0100

    Changed type field

--- src/de/lmu/ifi/bio/crco/data/genome/Transcript.java
+++ src/de/lmu/ifi/bio/crco/data/genome/Transcript.java
@@ -6,12 +6,10 @@ import java.util.List;
 import de.lmu.ifi.bio.crco.data.Entity;
 
 public class Transcript extends Entity{
-	enum TranscriptType{
-		ProteinCoding, OTHER
-	}
+	
 	private List<Exon> exons = null;
 	private String transcriptName;
-	private TranscriptType type;
+	private String type;
 	private Gene parentGene;
 	private Protein protein;
 	
@@ -23,7 +21,7 @@ public class Transcript extends Entity{
 		
 	}
 	
-	public Transcript(Gene parentGene, String transcriptName,TranscriptType type) {
+	public Transcript(Gene parentGene, String transcriptName,String type) {
 		super(transcriptName);
 		this.transcriptName = transcriptName;
 		this.parentGene = parentGene;
@@ -34,24 +32,39 @@ public class Transcript extends Entity{
 	public Gene getParentGene() {
 		return parentGene;
 	}
-
+	public String getType(){
+		return type;
+	}
 	public void setProtein(Protein currentProtein) {
 		this.protein = currentProtein;
 	}
 	public void addExon(Exon exon){
 		this.exons.add(exon);
 	}
+	/**
+	 * Returns the TSS end position (strand corrected)
+	 * @return the TSS end position or null when no exon or parent gene annotations are available
+	 */
+	public Integer getTSSEnd(){
+		if ( exons != null && parentGene != null){
+			if ( parentGene.getStrand().equals(Strand.PLUS)) //exons are ordered
+				return exons.get(exons.size()-1).getEnd();  //last exon end => TSS end
+			else
+				return exons.get(exons.size()-1).getStart(); //last exon start => TSS end (because of MINUS strand!)
+		}
+		return null;
+	}
 	
 	/**
-	 * Returns the TSS position
-	 * @return the TSS position or null when no exon or parent gene annotations are available
+	 * Returns the TSS start position (strand corrected)
+	 * @return the TSS start position or null when no exon or parent gene annotations are available
 	 */
-	public Integer getStart(){
+	public Integer getTSSStart(){
 		if ( exons != null && parentGene != null){
-			if ( parentGene.getStrand().equals(Strand.PLUS))
-				return exons.get(0).getStart();
+			if ( parentGene.getStrand().equals(Strand.PLUS)) //exons are ordered
+				return exons.get(0).getStart();  //first exon start => TSS start
 			else
-				return exons.get(0).getEnd();
+				return exons.get(0).getEnd(); //first exon end => TSS start (because of MINUS strand!)
 		}
 		return null;
 	}
