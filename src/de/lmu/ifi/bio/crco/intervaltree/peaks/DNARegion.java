80e607474d38e3a59f551981471b844b53fe655e
Robert Pesch <robert.pesch@bio.ifi.lmu.de>
2013-12-23 18:44:30 +0100

    up

--- src/de/lmu/ifi/bio/crco/intervaltree/peaks/DNARegion.java
+++ src/de/lmu/ifi/bio/crco/intervaltree/peaks/DNARegion.java
@@ -1,21 +1,14 @@
 package de.lmu.ifi.bio.crco.intervaltree.peaks;
 
-import de.lmu.ifi.bio.crco.data.genome.Gene;
 
 
 
 public class DNARegion extends Peak{
 
-	public Gene getGene(){
-		return gene;
-	}
-	
-	private Gene gene;
 	private String chrom;
-	public DNARegion(String chrom, Gene gene, int start, int end) {
+	public DNARegion(String chrom, int start, int end) {
 		super(start,end);
 		this.chrom = chrom;
-		this.gene = gene;
 	}
 	public String getChrom() {
 		return chrom;
--- src/de/lmu/ifi/bio/crco/intervaltree/peaks/TFBSPeak.java
+++ src/de/lmu/ifi/bio/crco/intervaltree/peaks/TFBSPeak.java
@@ -1,44 +1,47 @@
 package de.lmu.ifi.bio.crco.intervaltree.peaks;
 
+import java.util.List;
+
 import de.lmu.ifi.bio.crco.data.Entity;
 import de.lmu.ifi.bio.crco.data.genome.Transcript;
-
-
+import de.lmu.ifi.bio.crco.util.StringUtil;
 
 public class TFBSPeak extends Peak {
 	
-	private Entity factor;
+	private List<Entity> factors;
 	private Transcript target;
-	
-	private String chrom;
+	private String motifId;
+	//Distance from TFBS to annotated TSS (strand corrected; <0 Upstream; >0 Downstream)
 	private Integer distanceToTranscript = null;
 	private float pValue;
-	
 	
 	public float getpValue() {
 		return pValue;
 	}
 
-	public String getChrom() {
-		return chrom;
+	public List<Entity> getFactors(){
+		return factors;
 	}
-	
-	public Entity getFactor(){
-		return factor;
-	}
-	public Entity getTarget(){
+	public Transcript getTarget(){
 		return target;
 	}
 	
-	public TFBSPeak(String chrom,Entity factor,Transcript target,Integer distanceToTranscript, Float pValue, int start, int end) {
-		super(start, end);
-		this.chrom = chrom;
+	public TFBSPeak(String chrom,List<Entity> factors,String motifId,Transcript target,Integer distanceToTranscript, Float pValue, Float score, int start, int end) {
+		super(chrom,start, end,score);
 		this.target = target;
-		this.factor = factor;
+		this.factors = factors;
 		this.pValue = pValue;
+		this.motifId = motifId;
 		this.distanceToTranscript = distanceToTranscript;
 	}
 	
+	public String getMotifId() {
+		return motifId;
+	}
+
+	public Integer getDistanceToTranscript() {
+		return distanceToTranscript;
+	}
 
 	public TFBSPeak(int start, int end){
 		super(start,end);
@@ -46,9 +49,9 @@ public class TFBSPeak extends Peak {
 	
 	@Override
 	public String toString(){
-		return String.format("%s\t%s\t%s\t%s\t%s\t%s\t%d\t%f\t%d\t%d\n",
-				factor.getName(), factor.getIdentifier(), target.getIdentifier() , target.getParentGene().getIdentifier(),
-				getChrom() ,distanceToTranscript,pValue,getStart(),getEnd() );
+		return String.format("%s\t%s\t%s\t%d\t%f\t%f\t%s\t%d\t%d\n",
+				StringUtil.getAsList(factors, ','), target.getIdentifier() , target.getParentGene().getIdentifier(),
+				 distanceToTranscript,score,pValue,getChrom(),getStart(),getEnd() );
 	}
 
 
