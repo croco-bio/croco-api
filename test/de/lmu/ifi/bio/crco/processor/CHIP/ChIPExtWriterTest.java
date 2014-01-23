a35f77e12b053791c8e11c4a5125ccbee070d338
Robert Pesch <robert.pesch@bio.ifi.lmu.de>
2014-01-23 19:12:25 +0100

    updated tests

--- test/de/lmu/ifi/bio/crco/processor/CHIP/ChIPExtWriterTest.java
+++ test/de/lmu/ifi/bio/crco/processor/CHIP/ChIPExtWriterTest.java
@@ -7,10 +7,13 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import de.lmu.ifi.bio.crco.data.Entity;
 import de.lmu.ifi.bio.crco.data.genome.Gene;
 import de.lmu.ifi.bio.crco.intervaltree.Interval;
 import de.lmu.ifi.bio.crco.intervaltree.IntervalTree;
+import de.lmu.ifi.bio.crco.intervaltree.peaks.Peak;
 import de.lmu.ifi.bio.crco.intervaltree.peaks.Promoter;
+import de.lmu.ifi.bio.crco.intervaltree.peaks.TFBSPeak;
 import de.lmu.ifi.bio.crco.test.IntegrationTest;
 import de.lmu.ifi.bio.crco.util.FileUtil;
 import de.lmu.ifi.bio.crco.util.GenomeUtil;
@@ -18,6 +21,22 @@ import de.lmu.ifi.bio.crco.util.GenomeUtil;
 @Category(IntegrationTest.class)
 public class ChIPExtWriterTest {
 
+	@Test
+	public void testMouse() throws Exception{
+		List<Gene> genes = FileUtil.getGenes(new File("data/GeneAnnotation/Mus_musculus.NCBIM37.67.gtf"), null, null);
+		HashMap<String,IntervalTree<Promoter>> promoterTrees = GenomeUtil.createPromoterIntervalTree(genes,5000,5000,false);
+		File exp = new File("/home/users/pesch/Databases/ENCODE/CHIPSEQ-PEAKS/Mouse/wgEncodeSydhTfbs/wgEncodeSydhTfbsCh12Znfmizdcp1ab65767IggrabPk.narrowPeak.gz");
+		
+		HashMap<String, IntervalTree<Peak>> peaks = GenomeUtil.createPeakIntervalTree(exp,0,1,2,-1,null); //max size == we want to ignore very long peaks
+		List<TFBSPeak> targets = ChIPExtWriter.getTFBSPeaks(new Entity("test"),"test",peaks,promoterTrees, "chr",new HashMap<String,String>());
+		for(TFBSPeak tfbsPeak  : targets){
+			Entity factor = tfbsPeak.getFactors().get(0); //can be only 1
+			Entity target = new Entity( tfbsPeak.getTarget().getParentGene().getIdentifier());
+			
+			System.out.println(target);
+		}
+	}
+	
 	@Test
 	public void testWorm() throws Exception {
 
--- test/de/lmu/ifi/bio/crco/util/FileUtilTest.java
+++ test/de/lmu/ifi/bio/crco/util/FileUtilTest.java
@@ -11,7 +11,32 @@ import java.util.Set;
 
 import org.junit.Test;
 
+import de.lmu.ifi.bio.crco.data.genome.Gene;
+import de.lmu.ifi.bio.crco.util.FileUtil.MappingFileReader;
+
 public class FileUtilTest {
+	@Test
+	public void testReadGTF() throws Exception{
+		File gtf = new File("data/GeneAnnotation/Stat1.gtf.test");
+		
+		List<Gene> genes = FileUtil.getGenes(gtf, "protein_coding", null);
+		assertEquals(1,genes.size());
+		System.out.println(genes.get(0).getTranscripts());
+	}
+	
+	@Test
+	public void testMappingFileReader() throws Exception{
+		File tmpMappingFile = File.createTempFile("croco", ".tmp");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(tmpMappingFile));
+		bw.write("ID1\ta\tb\tc\n");
+		bw.write("ID2\td\n");
+		
+		bw.flush();
+		bw.close();
+		
+		HashMap<String, Set<String>> mapping = FileUtil.mappingFileReader(0,1, tmpMappingFile).includeAllColumnsAfterToIndex(true).readNNMappingFile();
+		System.out.println(mapping);
+	}
 	
 	@Test
 	public void testFileLookUp() throws Exception{
@@ -51,8 +76,6 @@ public class FileUtilTest {
 		assertEquals(mappingN1.size(),1);
 		
 		tmpMappingFile.delete();
-		
-		
 		
 	}
 
