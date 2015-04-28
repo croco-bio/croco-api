1a6469b4408816fabb883be4173cdc5510b1fdfc
Robert Pesch <robert.pesch@bio.ifi.lmu.de>
2015-04-28 18:11:55 +0200

    Added ChIP peak interval

---
+++ src/de/lmu/ifi/bio/croco/intervaltree/peaks/ChIP.java
@@ -0,0 +1,9 @@
+package de.lmu.ifi.bio.croco.intervaltree.peaks;
+
+public class ChIP extends Peak {
+
+    public ChIP(String chrom, int start, int end, Float score) {
+        super(chrom, start, end, score);
+    }
+
+}
